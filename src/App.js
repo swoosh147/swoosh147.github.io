960529e42280511a6ee3b2a6f4e006192b33fbfa
Max Savinov <maxisavinov147@gmail.com>
2020-05-28 13:14:24 +0300

    update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-            <h1>Ghbdtn</h1>
+            <h1>coming soon</h1>
       </header>
     </div>
   );
