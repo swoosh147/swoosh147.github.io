36cd678a6f404035aee29f5204485957e00bf8da
Max Savinov <maxisavinov147@gmail.com>
2020-05-28 12:10:37 +0300

    Updates

---
+++ precache-manifest.585970af3da27095c75989681275ead0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2950e41c138e18492d301c011284c61f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55cc98c844f5e9ecff0b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9c811b367011d04dacdf",
+    "url": "/static/js/2.7084dc58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7084dc58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55cc98c844f5e9ecff0b",
+    "url": "/static/js/main.daa44ae4.chunk.js"
+  },
+  {
+    "revision": "683c467fa8976be828c5",
+    "url": "/static/js/runtime-main.5f23b36b.js"
+  }
+]);
